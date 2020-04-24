f45a2e474b5ab3d4b4853ec78fe1031d2641e6f7
Pitrow <P1tr0w@users.noreply.github.com>
2020-04-24 12:25:09 +0200

    ReservaEspectaculoController terminado

--- src/main/java/com/erutnecca/xisney/repositories/ReservaEspectaculoRepository.java
+++ src/main/java/com/erutnecca/xisney/repositories/ReservaEspectaculoRepository.java
@@ -1,12 +1,16 @@
 package com.erutnecca.xisney.repositories;
 
+import com.erutnecca.xisney.entities.ReservaEspectaculo;
 import org.springframework.data.repository.CrudRepository;
 
-import com.erutnecca.xisney.entities.ReservaEspectaculo;
+import java.util.List;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface ReservaEspectaculoRepository extends CrudRepository<ReservaEspectaculo, Integer> {
+    List<ReservaEspectaculo> findByIdEvento(int id);
+
+    List<ReservaEspectaculo> findByIdUsuario(int id);
 
 }

c04f4758c4936aca8afedd13af3b5ea70d0fc187
Kotka <kotkataivaan@hotmail.com>
2020-06-04 22:57:53 +0200

    Eliminado otro import no usado

--- src/main/java/com/erutnecca/xisney/repositories/AvisoRepository.java
+++ src/main/java/com/erutnecca/xisney/repositories/AvisoRepository.java
@@ -3,7 +3,5 @@ package com.erutnecca.xisney.repositories;
 import com.erutnecca.xisney.entities.Aviso;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface AvisoRepository extends CrudRepository<Aviso, Integer> {
 }
